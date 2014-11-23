1c18a8c1d6de16408e83072b423c2512b836375b
Sangzi Liang <liangsztomo@gmail.com>
2014-11-23 12:16:29 -0500

    Implemented R script and completed readme

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 cleaningData
 ============
 
-Course project for coursera course "getting and cleaning data"
+This repo contains two files
+
+    run_analysis.R
+        
+        R script that merges and cleans motion sensor data
+        
+        * The "UCI HAR Dataset" folder needs to be in the R work directory
+        * Run the script by sourcing it: source("run_analysis.R")
+        * Produces a tidy dataset in "data.txt"
+        * Leaves the cleaned data before (data) and after (aggdata) averaging in the R environment
+        * Read the dataset into R by: read.table("data.txt", header = TRUE)
+
+    CodeBook.md
+    
+        Description of the variables in the dataset and cleaning procedures
---
+++ run_analysis.R
@@ -0,0 +1,55 @@
+## This is an R script for merging and cleaning motion sensor data from
+## https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+## The data is downloaded and the "UCI HAR Dataset" folder is in the work directory
+
+## Read in all the relevant files
+
+subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
+
+subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
+
+## Merge the subject labels, sensor data, and activity labels
+
+test_data <- cbind(subject_test, X_test, y_test)
+train_data <- cbind(subject_train, X_train, y_train)
+
+## Merge the training and test data sets
+
+data <- rbind(test_data, train_data)
+
+## Read in the feature names
+
+feature <- read.table("UCI HAR Dataset/features.txt")
+
+## Assign column names to data
+
+colnames(data) <- c("subject-id", as.character(feature[[2]]), "activity")
+
+## Construct a logical vector and select the needed columns
+## fixed = TRUE to exclude the meanFreq columns
+
+index <- grepl("mean()", colnames(data), fixed = TRUE) | grepl("std()", colnames(data))
+index[1] <- TRUE
+index[563] <- TRUE
+data <- data[,index]
+
+## Replace the activity with descriptive names using factors
+
+act<- read.table("UCI HAR Dataset/activity_labels.txt")
+data$activity <- factor(data$activity, c(1,2,3,4,5,6), as.character(act[[2]]))
+
+## Remove data that are no longer useful
+
+remove(subject_test, X_test, y_test)
+remove(subject_train, X_train, y_train)
+remove(test_data, train_data, feature, act, index)
+
+## Compute average for each variable, subject, and activity
+aggdata <- aggregate(data[, -c(1,68)], by = list(`subject-id` = data$`subject-id`, 
+                                        activity = data$activity), FUN = mean)
+
+write.table(aggdata, "data.txt", row.names = FALSE)
